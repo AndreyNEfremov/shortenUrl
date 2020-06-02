3b8be225d26ebb84ac8567ea5731e4865520fe83
AndreyNEfremov <postandry@gmail.com>
2020-06-02 21:55:15 +0300

    authPage

--- Client/src/pages/AuthPage.jsx
+++ Client/src/pages/AuthPage.jsx
@@ -2,8 +2,42 @@ import React from 'react';
 
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className='row'>
+            <div className="col s6 offset-s3">
+                <h1>Shorten URL</h1>
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Login</span>
+                        <div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Your e-mail"
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    className='yellow-input'
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Your password"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    className='yellow-input'
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        {/* стайл делать только в css, это учебная демонстрация */}
+                        <button className='btn yellow darken-4' style={{marginRight: 20}}>Sign in</button>
+                        <button className='btn grey lighten-1 black-text'>Sign up</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
