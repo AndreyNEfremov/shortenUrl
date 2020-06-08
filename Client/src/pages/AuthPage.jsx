4bf303dc467ea7ab3d508faec9bc98ba23ca917a
AndreyNEfremov <postandry@gmail.com>
2020-06-08 23:48:51 +0300

    login

--- Client/src/pages/AuthPage.jsx
+++ Client/src/pages/AuthPage.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useHttp} from "../hooks/http.hook";
 import {useMessage} from "../hooks/message.hook";
+import {AuthContext} from "../context/AuthContext";
 
 
 export const AuthPage = () => {
+    const auth = useContext(AuthContext);
     const message = useMessage();
     const {loading, error, request, clearError} = useHttp();
     const [form, setForm] = useState({
@@ -25,6 +27,15 @@ export const AuthPage = () => {
             const data = await request('/api/auth/register', 'POST', {...form});
             // console.log('Data', data)
             message(data.message)
+        } catch (e) {
+
+        }
+    };
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form});
+            auth.login (data.token, data.userId)
         } catch (e) {
 
         }
@@ -66,12 +77,15 @@ export const AuthPage = () => {
                         {/* стайл делать только в css, это учебная демонстрация */}
                         <button className='btn yellow darken-4'
                                 style={{marginRight: 20}}
-                                disabled={loading}>
+                                onClick={loginHandler}
+                                disabled={loading}
+                        >
                             Sign in
                         </button>
                         <button className='btn grey lighten-1 black-text'
                                 onClick={registerHandler}
-                                disabled={loading}>
+                                disabled={loading}
+                        >
                             Sign up
                         </button>
                     </div>
