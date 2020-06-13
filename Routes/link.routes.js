5d904258fd283f1644522e8557193999680ac122
AndreyNEfremov <postandry@gmail.com>
2020-06-13 18:22:57 +0300

    link_route_p2

--- Routes/link.routes.js
+++ Routes/link.routes.js
@@ -1,13 +1,33 @@
 const {Router} = require('express'); //роутер
+const config = require('config');
+const shortid = require('shortid');
 const Link = require('../Models/Link');
 const auth = require('../Middleware/auth.middleware');
 const router = Router(); //константа роута
 
 
 router.post(
-    '/generate', //генерируем короткий урл
+    '/generate', auth, //генерируем короткий урл
     async (req, res) => {
         try {
+            const baseUrl = config.get('baseUrl');
+            const {from} = req.body;
+
+            const code = shortid.generate();
+
+            const existing = await Link.findOne({from});
+            if (existing) {
+                return res.json({link: existing})
+            }
+
+            const to = baseUrl + '/t/' + code;
+
+            const link = new Link({
+                code, to, from, owner: req.user.userId
+            });
+
+            await link.save();
+            res.status(201).json({link})
 
         } catch (e) {
             res.status(500).json({message: 'Link post - Something went wrong, try again'})
@@ -27,7 +47,7 @@ router.get(
     });
 
 router.get(
-    '/:id', //получаем ссылку по айди
+    '/:id', auth, //получаем ссылку по айди
     async (req, res) => {
         try {
             const link = await Link.findById(req.params.id);
